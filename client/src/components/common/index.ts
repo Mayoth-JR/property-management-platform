a558c688ee03ceb36db710585cd3d3fbc77af615
Aminiely Samwel <156130246+Mayoth-JR@users.noreply.github.com>
2026-06-09 16:33:04 +0300

    Create index.ts

---
+++ client/src/components/common/index.ts
@@ -0,0 +1,9 @@
+// src/components/common/index.ts
+export { Button } from './Button';
+export { Card } from './Card';
+export { Input } from './Input';
+export { Select } from './Select';
+export { Modal } from './Modal';
+export { Alert } from './Alert';
+export { Table } from './Table';
+export { Pagination } from './Pagination';
